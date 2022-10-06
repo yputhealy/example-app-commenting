2b5624787acae5798885ba13e5845c4566d37bf8
Y Puthealy <108327554+yputhealy@users.noreply.github.com>
2022-10-06 16:48:27 +0700

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -11,7 +11,7 @@ from googleapiclient.http import HttpRequest
 socket.setdefaulttimeout(15 * 60)
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
+SPREADSHEET_ID = "1MnumEsX-XeyJYI7FkXeSvdfbcEZlJV0ssMTi0fc2xjM/edit?usp=sharing"
 SHEET_NAME = "Database"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
